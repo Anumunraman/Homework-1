f1238854e2687df0206dafd2557e271ca1e18286
nikolaev mikhail <6n2n69@gmail.com>
2022-06-26 00:27:17 +0300

    task 1

---
+++ task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter two numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
