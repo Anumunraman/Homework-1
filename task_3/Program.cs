aadf8d1b23416c399795c2b7f2ee571690fb1ee7
nikolaev mikhail <6n2n69@gmail.com>
2022-06-26 01:00:48 +0300

    task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.Write(a);
+    Console.WriteLine(" -> YES");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" -> NO");
+}
